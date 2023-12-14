72e37c54cc8f304eceb81e12d044111107fc7031
Sebastian Hondarza <sebastian.hondarza@gmail.com>
2023-12-14 00:09:29 -0300

    Update langchain.chains and langchain.memory imports

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from apiKey import apiKey
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SimpleSequentialChain
+from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
 
 os.environ['OPENAI_API_KEY'] = apiKey
 
@@ -24,16 +25,22 @@ script_template = PromptTemplate(
     template='write me a youtube video script based on this title TITLE: {title}'
 )
 
-
+# Memory
+memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
 
 #Llms
 llm = OpenAI(temperature=0.9)
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
-script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True)
-sequential_chain = SimpleSequentialChain(chains=[title_chain, script_chain], verbose=True)
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=memory)
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=memory)
+sequential_chain = SequentialChain(chains=[title_chain, script_chain], input_variables=['topic'],
+                                   output_variables=['title', 'script'], verbose=True)
 
 
 #show stuff to the screen if theres a prompt
 if prompt:
-    response = sequential_chain.run(prompt)
-    st.write(response)
+    response = sequential_chain({'topic': prompt})
+    st.write(response['title'])
+    st.write(response['script'])
+
+    with st.expander('Message History'):
+        st.info(memory.buffer)
